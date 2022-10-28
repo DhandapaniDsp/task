9fe1ba68ce4e0866df4b4d47e3c4a7e4ec486f20
Dhandapani venkatesan <dhandapani@crayond.co>
2022-10-28 17:19:25 +0530

    five

---
+++ script.js
@@ -0,0 +1,91 @@
+ 
+        function cardOne(x) {
+            document.getElementById("onehov").style.display = "block";
+            document.getElementById("one").style.display = "none";
+        }
+
+        function cardHovOne(x) {
+            document.getElementById("onehov").style.display = "none";
+            document.getElementById("one").style.display = "block";
+        }
+        function cardTwo(x) {
+            document.getElementById("twohov").style.display = "block";
+            document.getElementById("two").style.display = "none";
+        }
+
+        function cardHovTwo(x) {
+            document.getElementById("twohov").style.display = "none";
+            document.getElementById("two").style.display = "block";
+        }
+        function cardThree(x) {
+            document.getElementById("threehov").style.display = "block";
+            document.getElementById("three").style.display = "none";
+        }
+
+        function cardHovThree(x) {
+            document.getElementById("threehov").style.display = "none";
+            document.getElementById("three").style.display = "block";
+        }
+        function cardFour(x) {
+            document.getElementById("fourhov").style.display = "block";
+            document.getElementById("four").style.display = "none";
+        }
+
+        function cardHovFour(x) {
+            document.getElementById("fourhov").style.display = "none";
+            document.getElementById("four").style.display = "block";
+        }
+        function cardFive(x) {
+            document.getElementById("fivehov").style.display = "block";
+            document.getElementById("five").style.display = "none";
+        }
+
+        function cardHovFive(x) {
+            document.getElementById("fivehov").style.display = "none";
+            document.getElementById("five").style.display = "block";
+        }
+        function cardSix(x) {
+            document.getElementById("sixhov").style.display = "block";
+            document.getElementById("six").style.display = "none";
+        }
+
+        function cardHovSix(x) {
+            document.getElementById("sixhov").style.display = "none";
+            document.getElementById("six").style.display = "block";
+        }
+        function cardSeven(x) {
+            document.getElementById("sevenhov").style.display = "block";
+            document.getElementById("seven").style.display = "none";
+        }
+
+        function cardHovSeven(x) {
+            document.getElementById("sevenhov").style.display = "none";
+            document.getElementById("seven").style.display = "block";
+        }
+        function cardEight(x) {
+            document.getElementById("eighthov").style.display = "block";
+            document.getElementById("eight").style.display = "none";
+        }
+
+        function cardHovEight(x) {
+            document.getElementById("eighthov").style.display = "none";
+            document.getElementById("eight").style.display = "block";
+        }
+        function cardNine(x) {
+            document.getElementById("ninehov").style.display = "block";
+            document.getElementById("nine").style.display = "none";
+        }
+
+        function cardHovNine(x) {
+            document.getElementById("ninehov").style.display = "none";
+            document.getElementById("nine").style.display = "block";
+        }
+             function profile(x) {
+            document.getElementById("prof").style.display = "block";
+            document.getElementById("btn").style.display = "none";
+        }
+
+        function create(x) {
+            document.getElementById("prof").style.display = "none";
+            document.getElementById("btn").style.display = "block";
+        }
